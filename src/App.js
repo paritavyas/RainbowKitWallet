da3bee879c126d373c6271a0e41bc1664aceba69
paritavyas <paritavyas.vhits@gmail.com>
2023-07-18 11:10:26 +0530

    blockchain

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  zora,
+} from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+import Home from './Home';
+
+
+const { chains, publicClient } = configureChains(
+  [mainnet, polygon, optimism, arbitrum, zora],
+  [
+    alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
+    publicProvider()
+  ]
+);
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  projectId: 'YOUR_PROJECT_ID',
+  chains
+});
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient
+})
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <Home />
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
+
+function Home() {
+    return (
+        <>
+            <h1>hello</h1>
+            <ConnectButton/>
+        </>
+    );
+}
+
+export default Home;
